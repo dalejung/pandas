7736c7d669d008ba89e4813c1bb40ce8fe5e16c6
Dale Jung <dale@dalejung.com>
2013-09-18 09:41:08 -0400

    PRF: Adding Panels.shift test

---
+++ vb_suite/panel_methods.py
@@ -0,0 +1,16 @@
+from vbench.api import Benchmark
+from datetime import datetime
+
+common_setup = """from pandas_vb_common import *
+"""
+
+#----------------------------------------------------------------------
+# shift
+
+setup = common_setup + """
+index = date_range(start="2000", freq="D", periods=1000)
+panel = Panel(np.random.randn(100, len(index), 1000))
+"""
+
+panel_shift = Benchmark('panel.shift(1)', setup,
+                               start_date=datetime(2012, 1, 12))
