27a18e8e6c32d6987e8ad5b655ed0caf301b091d
Dale Jung <dale@dalejung.com>
2015-03-05 19:51:40 -0500

    Added python 2/3 cmopat metaclass method

--- pandas/compat/__init__.py
+++ pandas/compat/__init__.py
@@ -368,3 +368,14 @@ def is_platform_mac():
 
 def is_platform_32bit():
     return struct.calcsize("P") * 8 < 64
+
+# taken from six
+def with_metaclass(meta, *bases):
+    """Create a base class with a metaclass."""
+    # This requires a bit of explanation: the basic idea is to make a dummy
+    # metaclass for one level of class instantiation that replaces itself with
+    # the actual metaclass.
+    class metaclass(meta):
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+    return type.__new__(metaclass, 'temporary_class', (), {})
